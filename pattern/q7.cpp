3b207cae68456cd8481cc851f4aa457becd6cb19
Nite2005 <kumarnitesh140905@gmail.com>
2025-06-22 22:40:29 +0530

    pattern question

--- pattern/q7.cpp
+++ pattern/q7.cpp
@@ -7,15 +7,33 @@ int main()
     cout<<"Enter num of Rows: ";
     cin>>n;
 
-    for(int i=1;i<=n;i++)
+    // for(int i=1;i<=n;i++)
+    // {   int k = 1;
+    //     for(int j=1;j<=i;j++)
+    //     {
+    //          cout<<k;
+    //          k+=2;
+    //     }
+    //     cout<<endl;
+    // }
+
+
+    // second method 
+    for(int i =1 ;i<=n;i++)
     {
-        for(int j=1;j<=i;j++)
-        {
-             if(j==1)cout<<j;
-             else{
-                cout<<j+2;
-             }
-        }
-        cout<<endl;
+        for(int j=1 ;j<=2*i-1;j+=2)
+      {
+        cout<<j;
+      }
+      cout<<endl;
     }
+
+
 }
+
+
+// // pattern
+// 1
+// 13
+// 135
+// 1357
