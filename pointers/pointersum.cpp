6bbf9eae39d481e4d3fcaaf6c5fc353881a1b244
Nite2005 <kumarnitesh140905@gmail.com>
2025-07-08 17:26:38 +0530

    sum of two number using pointer

---
+++ pointers/pointersum.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+     int x ,y ;
+
+     cout<<"enter the value of x";
+     cin>>x;
+
+     cout<<"Enter the value of y";
+     cin>>y;
+
+
+     int *p = &x;
+     int *p1 = &y;
+
+     cout<<"sum of two number is : "<<*p+*p1<<endl;
+}
