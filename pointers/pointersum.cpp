453977fdf4f2ec712453e2b3fb580f48789423ee
Nite2005 <kumarnitesh140905@gmail.com>
2025-07-08 17:30:50 +0530

    taking input using pointer

--- pointers/pointersum.cpp
+++ pointers/pointersum.cpp
@@ -3,17 +3,28 @@ using namespace std;
 
 int main()
 {
-     int x ,y ;
+    //  int x ,y ;
 
-     cout<<"enter the value of x";
-     cin>>x;
+    //  cout<<"enter the value of x ";
+    //  cin>>x;
 
-     cout<<"Enter the value of y";
-     cin>>y;
+    //  cout<<"Enter the value of y ";
+    //  cin>>y;
+
+    //  int *p = &x;
+    //  int *p1 = &y;
+    //  cout<<"sum of two number is : "<<*p+*p1<<endl;
 
 
-     int *p = &x;
-     int *p1 = &y;
+    int x;
+    int *p = &x;
+    cout<<"Enter the value of x : ";
+    cin>>*p;  // Taking input using pointer 
 
-     cout<<"sum of two number is : "<<*p+*p1<<endl;
+    int y ;
+    int *p1 = &y;
+    cout<<"Enter the value of y : ";
+    cin>>*p1;  // Taking the input of y using pointer 
+
+    cout<<"sum of two number is : "<<*p1 + *p<<endl;
 }
