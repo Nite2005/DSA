4ed3cf0c580127be381b57ad57c18c84bad42528
Nite2005 <kumarnitesh140905@gmail.com>
2025-07-08 19:14:03 +0530

    arithmetic.cpp

---
+++ pointers/arithmetic.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int x = 4;
+    int *p = &x;
+
+    // cout<<*p<<endl;
+    // p = p + 1;
+    // cout<<*p<<endl;
+
+    // cout<<p<<endl;
+    // p = p+1;
+    // cout<<p<<endl;// it will increment by size of datatype
+
+    // cout<<(*p)++<<endl;
+    // cout<<*p<<endl;
+
+    // cout<<*p++<<endl;
+    // cout<<*p<<endl;//wrong
+
+
+
+}
