25618415b629c3c5e8f37981c2795c14aa1df66c
Nite2005 <kumarnitesh140905@gmail.com>
2025-07-14 18:57:51 +0530

    vector basic

---
+++ array/vectorsbasic.cpp
@@ -0,0 +1,68 @@
+// vectors -> array ek fixed size ki property ko follow karta hai jiski wajah se ham array me 
+// ek limit size ke baad element insert nhi kar paate hai isi problem ko solve karta h vector , 
+// vector ek dynamic array hai isme jab hm element insert karte hai tab ye ek dusra allag array 
+// create kar deta h double size ka iske baad usme element insert hota rehta h 
+
+
+#include<iostream>
+#include<vector>
+using namespace std;
+
+
+int main()
+{
+    vector<char>arr;
+    
+    // cout<<arr.capacity()<<endl;
+    // arr.push_back('A');
+    // cout<<arr[0]<<" "<<arr.capacity()<<endl;
+    // arr.push_back('B');
+    // cout<<arr[1]<<" "<<arr.capacity()<<endl;
+    // arr.push_back('C');
+    // cout<<arr[1]<<" "<<arr.capacity()<<endl;
+    // arr.push_back('D');
+    // cout<<arr[1]<<" "<<arr.capacity()<<endl;
+    // arr.push_back('E');
+    // cout<<arr[1]<<" "<<arr.capacity()<<endl;
+    // arr.push_back('F');
+    // cout<<arr[1]<<" "<<arr.capacity()<<endl;
+    // arr.push_back('G');
+    // cout<<arr[1]<<" "<<arr.capacity()<<endl;
+    // arr.push_back('H');
+    // cout<<arr[1]<<" "<<arr.capacity()<<endl;
+    // arr.push_back('I');
+    // cout<<arr[1]<<" "<<arr.capacity()<<endl;
+    // arr.push_back('J');
+    // cout<<arr[1]<<" "<<arr.capacity()<<endl;
+    // arr.push_back('K');
+    // cout<<arr[1]<<" "<<arr.capacity()<<endl;
+    // arr.push_back('L');
+    // cout<<arr[1]<<" "<<arr.capacity()<<endl;
+    // arr.push_back('M');
+    // cout<<arr[1]<<" "<<arr.capacity()<<endl;
+    // arr.push_back('N');
+    // cout<<arr[1]<<" "<<arr.capacity()<<endl;
+    // arr.push_back('O');
+    // cout<<arr[1]<<" "<<arr.capacity()<<endl;
+    // arr.push_back('P');
+    // cout<<arr[1]<<" "<<arr.capacity()<<endl;
+
+
+
+   
+// size of vector  -> ye hame ye batata h ki vector total kitna element h 
+// capacity of vector -> ye hame batata h ki total kitna element add ho sakta aur ye dynamically jo capacity 
+//   full ho jata h aur hme aur element add karna hota h tab vector ka capacity present ke capacity se double ho jata h 
+
+
+   
+
+    // for(int i=0;i<arr.size();i++)
+    // {
+    //     cout<<arr[i]<<" ";
+
+    // }
+
+
+    
+}
